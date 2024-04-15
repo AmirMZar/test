474b6729a956e6e9c7fcdad2c11f48d4caf3bf89
Amir <a313gputimal@gmail.com>
2024-04-15 20:25:39 +0330

    structor

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,14 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Routes from './rout/routes'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <p> ghjhgvjf </p>
-      <h1> 1 </h1>
-      <h2> 2 </h2>
-      <p> dfgjjvjf </p>
+      <Routes />
     </>
   )
 }
